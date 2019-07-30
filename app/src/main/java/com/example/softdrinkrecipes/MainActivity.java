0c5714c0e5785d48c64a06594e7cbe34af41e61c
uche-hub <ucj.theUche@gmail.com>
2019-07-30 15:18:11 +0000

    Soft-Drink-Recipes commit

---
+++ app/src/main/java/com/example/softdrinkrecipes/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.softdrinkrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+
+    public final static String EXTRA_MESSAGE = "com.example.softdrinkrecipes.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
