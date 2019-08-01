9a61913a2386baddb5c0321bfd654ea748786d3a
uche-hub <ucj.theUche@gmail.com>
2019-08-01 12:00:15 +0000

    i Change the background image And i also change the text

--- app/src/main/java/com/example/softdrinkrecipes/Home.java
+++ app/src/main/java/com/example/softdrinkrecipes/Home.java
@@ -2,7 +2,9 @@ package com.example.softdrinkrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void softDrinks(View v){
+        Intent intent = new Intent(this, SoftDrinks.class);
+        startActivity(intent);
     }
 }
